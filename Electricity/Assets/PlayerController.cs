35723cff244b6845abb36e1ce241847b33cfcc08
luoguangbo <d2269690638@qq.com>
2020-10-14 23:17:56 +0800

    无数次重启，这就作为最终版的初始吧

---
+++ Electricity/Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+    public float jumpforce;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Movement();
+    }
+    void Movement()
+    {
+        float horizontalmove = Input.GetAxis("Horizontal");
+        float facedircetion = Input.GetAxisRaw("Horizontal");
+
+        if (horizontalmove != 0)
+        {
+            rb.velocity = new Vector2(horizontalmove * speed * Time.deltaTime, rb.velocity.y);
+        }
+        if(facedircetion != 0)
+        {
+            transform.localScale = new Vector3(facedircetion, 1, 1);
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpforce * Time.deltaTime);
+        }
+    }
+}
